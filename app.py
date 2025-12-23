8984c4fdc2d2f4f0f600615c974727250dad6770
Mru00-hub <mrudulabhalke75225@gmail.com>
2025-12-23 16:09:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,45 +14,6 @@ MEDICAL_DISCLAIMER = """
 
 import math
 
-class FLUID_LIBRARY:
-    """
-    The Pharmacopoeia of Fluids. 
-    Defines how different fluids behave physically.
-    """
-    SPECS = {
-        FluidType.RL: FluidProperties(
-            name="Ringer Lactate", 
-            sodium_meq_l=130, glucose_g_l=0, oncotic_pressure_mmhg=0, 
-            vol_distribution_intravascular=0.25 # Crystalloid: 1/4 stays, 3/4 leaks
-        ),
-        FluidType.NS: FluidProperties(
-            name="Normal Saline", 
-            sodium_meq_l=154, glucose_g_l=0, oncotic_pressure_mmhg=0, 
-            vol_distribution_intravascular=0.25
-        ),
-        FluidType.D5_NS: FluidProperties(
-            name="D5 Normal Saline", 
-            sodium_meq_l=154, glucose_g_l=50, oncotic_pressure_mmhg=0, 
-            vol_distribution_intravascular=0.20 # Glucose metabolizes -> free water -> cells
-        ),
-        FluidType.COLLOID_ALBUMIN: FluidProperties(
-            name="Albumin 5%", 
-            sodium_meq_l=145, glucose_g_l=0, oncotic_pressure_mmhg=20.0, # High Pull
-            vol_distribution_intravascular=1.0, # Stays in vessel
-            is_colloid=True
-        ),
-        FluidType.PRBC: FluidProperties(
-            name="Packed Red Blood Cells",
-            sodium_meq_l=140, glucose_g_l=0, oncotic_pressure_mmhg=25.0,
-            vol_distribution_intravascular=1.0,
-            is_colloid=True
-        )
-    }
-
-    @staticmethod
-    def get(fluid_enum: FluidType) -> FluidProperties:
-        return FLUID_LIBRARY.SPECS.get(fluid_enum, FLUID_LIBRARY.SPECS[FluidType.RL])
-
 class PediaFlowPhysicsEngine:
     """
     The Mathematical Core.
